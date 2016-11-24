bd8f6b63051ed94c654f04254c83d24c9b046747
bit-user <bit-user@bit>
2016-11-24 17:49:19 +0900

    asdasd

---
+++ app/src/main/java/com/bit2016/gugudanfighter/ResultActivity.java
@@ -0,0 +1,32 @@
+package com.bit2016.gugudanfighter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        findViewById(R.id.button_restart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.button_main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
